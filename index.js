77e098d2bbf6449a63e4641e59b6ce1841d925f1
Mofeid Moshreky <mof_ton@yahoo.com>
2023-03-06 14:30:59 -0500

    copmlete lap

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution here!
+function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+  
+  const cats = ["Milo", "Otis", "Garfield"];
+  
+  function destructivelyRemoveLastCat() {
+    cats.pop();
+  }
+  function appendCat(name) {
+    return [...cats, name];
+  }
+  
+  
+  function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+  
+  function removeLastCat() {
+    return cats.slice(0, -1);
+  }
+  
+  function destructivelyRemoveFirstCat() {
+    cats.shift();
+  }
+  
+  function prependCat(name) {
+    return [name, ...cats];
+  }
+   
+  
+  function removeFirstCat() {
+    return cats.slice(1);
+  }
